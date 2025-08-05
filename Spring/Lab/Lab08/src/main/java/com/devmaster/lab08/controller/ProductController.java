428d82357d2683226c5a7e64bafa0cf87cd85e04
Quang <quangquan23451822@gmail.com>
2025-08-05 14:13:07 +0700

    :3

--- Spring/Lab/Lab08/src/main/java/com/devmaster/lab08/controller/ProductController.java
+++ Spring/Lab/Lab08/src/main/java/com/devmaster/lab08/controller/ProductController.java
@@ -1,9 +1,14 @@
 package com.devmaster.lab08.controller;
 
+import com.devmaster.lab08.dto.ProductDTO;
+import com.devmaster.lab08.entity.Product;
 import com.devmaster.lab08.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,7 +19,15 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public String products() {
+    public String products(Model model) {
+        model.addAttribute("products", productService.findAll());
+        model.addAttribute("product", new Product());
         return "product";
+    }
+
+    @PostMapping("/products")
+    public String addProduct(@ModelAttribute ProductDTO product) {
+        productService.save(product);
+        return "redirect:/home/products";
     }
 }
